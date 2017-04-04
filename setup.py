2caecf158626ee058dc405cdb70ef7ac641bcdc0
Ilya Vihtinsky <vihtinsky@cloud.upwork.com>
2017-04-04 16:59:11 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='acl-rules',
+      version='0.5',
+      description='Declarative acl rules',
+      author='Akis Kesoglou, Ilya Vihtinsky',
+      author_email='ilia@vihtinsky.ru',
+      url='https://github.com/vihtinsky/acl-rules',
+      packages=['acl'],
+     )
+
